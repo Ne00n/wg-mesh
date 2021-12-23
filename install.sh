a477d53af59468041bcea8158ac2df732a0bdc08
Neoon <neo@tiefkuehler-madness.me>
2021-12-23 13:41:23 +0100

    need to install netaddr too

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-apt-get install wireguard bird2 python3 fping git -y
+apt-get install wireguard bird2 python3 python3-pip fping git -y
+pip3 install netaddr
 cd /opt/
 git clone https://github.com/Ne00n/wg-mesh.git
 cd wg-mesh
