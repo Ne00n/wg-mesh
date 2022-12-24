ef8c522634c1e4d139617339d56283792ff63eb0
Neoon <neo@tiefkuehler-madness.me>
2022-12-24 12:30:13 +0100

    add user + start api as service

--- install.sh
+++ install.sh
@@ -5,6 +5,22 @@ pip3 install netaddr
 cd /opt/
 git clone https://github.com/Ne00n/wg-mesh.git
 cd wg-mesh
+useradd wg-mesh -r -d /opt/wg-mesh -s /bin/bash
+chown -R wg-mesh:wg-mesh /opt/wg-mesh/
+chmod -R 700 /opt/wg-mesh/
+echo -e "[Unit]
+Description=wgmesh service
+Wants=network-online.target
+After=network-online.target
+[Service]
+User=wg-mesh
+Group=wg-mesh
+Type=simple
+WorkingDirectory=/opt/wg-mesh
+ExecStart=/usr/bin/python3 api.py
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/wgmesh.service
+systemctl enable wgmesh && systemctl start wgmesh
 if [ "$1" == "init" ];  then
 ./cli.py $@
 fi
