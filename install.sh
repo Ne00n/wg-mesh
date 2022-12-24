f4c9c030aedab4538459e29e77dd5477a7172366
Neoon <neo@tiefkuehler-madness.me>
2022-12-24 22:19:11 +0100

    sudo

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-apt-get install wireguard bird2 python3 python3-pip fping git -y
+apt-get install wireguard bird2 sudo python3 python3-pip fping git -y
 pip3 install netaddr
 pip3 install requests
 cd /opt/
+#git
 git clone https://github.com/Ne00n/wg-mesh.git
 cd wg-mesh
 useradd wg-mesh -r -d /opt/wg-mesh -s /bin/bash
 chown -R wg-mesh:wg-mesh /opt/wg-mesh/
 chmod -R 700 /opt/wg-mesh/
+#wireguard permissions
 chgrp wg-mesh /etc/wireguard/
 chmod 770 /etc/wireguard/
+#sudo permissions
+echo "wg-mesh ALL=(ALL) NOPASSWD: /bin/systemctl start wg-quick@*" >> /etc/sudoers
+echo "wg-mesh ALL=(ALL) NOPASSWD: /bin/systemctl stop wg-quick@*" >> /etc/sudoers
+#systemd service
 echo -e "[Unit]
 Description=wgmesh service
 Wants=network-online.target
