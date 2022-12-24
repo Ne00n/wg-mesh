8de1074d86c5cec3e1538a60f6fd4b7a058b99bb
Neoon <neo@tiefkuehler-madness.me>
2022-12-24 20:29:30 +0100

    fix

--- cli.py
+++ cli.py
@@ -4,14 +4,14 @@ from Class.wireguard import Wireguard
 from Class.bird import Bird
 import sys, os
 
-wg = Wireguard()
-
 if len(sys.argv) == 1:
     print("init <name> <id>")
 elif sys.argv[1] == "init":
     wg = Wireguard(True)
     wg.init(sys.argv[2],sys.argv[3])
 elif sys.argv[1] == "connect":
+    wg = Wireguard()
     wg.connect(sys.argv[2])
 elif sys.argv[1] == "disconnect":
+    wg = Wireguard()
     wg.disconnect()
