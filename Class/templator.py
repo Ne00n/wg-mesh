656a882b91022d46b2a18164ce929e3ce6904229
Neoon <me@tiefkuehler-madness.me>
2023-01-11 12:02:51 +0100

    remove genDummy

--- Class/templator.py
+++ Class/templator.py
@@ -29,21 +29,6 @@ fi'''
         template = ""
         for node in targets: template += f'bridge fdb append 00:00:00:00:00:00 dev vxlan251 dst 10.0.{node}.1;'
         return template
-
-    def genDummy(self,id,privateKey,targets=[]):
-        template = f'''[Interface]
-        Address = 127.11.11.11/32
-        ListenPort = 52820
-        PrivateKey = '''+str(privateKey)
-        template += f'\nPostUp =  echo 1 > /proc/sys/net/ipv4/ip_forward; echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter; echo 0 > /proc/sys/net/ipv4/conf/default/rp_filter; echo "fq" > /proc/sys/net/core/default_qdisc; echo "bbr" > /proc/sys/net/ipv4/tcp_congestion_control; ip addr add 10.0.{id}.1/30 dev lo;'
-        template += "iptables -t nat -A POSTROUTING -o $(ip route show default | awk '/default/ {print $5}') -j MASQUERADE;"
-        template += f'ip link add vxlan251 type vxlan id 251 dstport 4789 local 10.0.{id}.1; ip link set vxlan251 up;'
-        template += f'ip addr add 10.0.251.{id}/24 dev vxlan251;'
-        template += self.genVXLAN(targets)
-        template += f'\nPostDown = ip addr del 10.0.{id}.1/30 dev lo; ip link delete vxlan251;'
-        template += '''
-        SaveConfig = true'''
-        return template
     
     def getFirst(self,latency):
         for entry in latency: return entry
