ba76bfa6a641e327c1437d386c2fccbd81250420
Ne00n <me@tiefkuehler-madness.me>
2023-01-15 22:42:52 +0100

    remove systemd service files

--- deinstall.sh
+++ deinstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 systemctl disable wgmesh && systemctl stop wgmesh
+rm /etc/systemd/system/wgmesh.service
 systemctl disable wgmesh-bird && systemctl stop wgmesh-bird
+rm /etc/systemd/system/wgmesh-bird.service
 userdel -r wg-mesh
 rm /etc/sysctl.d/wg-mesh.conf
 rm /etc/sudoers.d/wg-mesh.conf
