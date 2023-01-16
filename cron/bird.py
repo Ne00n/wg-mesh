1cd35d2529d203ba84f03bcafe746e5cfd97c2b7
Neoon <neo@tiefkuehler-madness.me>
2023-01-16 08:09:26 +0100

    only run if links changed

--- cron/bird.py
+++ cron/bird.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-import time, sys
+import time, sys, os
 sys.path.append("..") # Adds higher directory to python modules path.
 from Class.bird import Bird
 
 bird = Bird()
 
+path = os.path.dirname(os.path.realpath(__file__))
+path = f'{path}/links/'.replace("/cron","")
+links = os.listdir(path)
+bird.bird()
+
 while True:
-    bird.bird()
+    currentLinks = os.listdir(path)
+    if links != currentLinks:
+        bird.bird()
+        links = currentLinks
     time.sleep(60)
