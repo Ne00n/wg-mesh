34c3c9f2b2cbe4410810e753f492ef021cff4eb9
Neoon <neo@tiefkuehler-madness.me>
2023-01-18 03:38:45 +0100

    update call

--- Class/base.py
+++ Class/base.py
@@ -7,15 +7,15 @@ class Base:
         return p.stdout.decode('utf-8')
 
     def call(self,url,payload):
-        for run in range(4):
-            print(f"Retrying {run+1} of 4")
+        for run in range(1,5):
             try:
                 req = requests.post(url, json=payload)
                 if req.status_code == 200: return req
                 print(f"Got {req.text} as response")
             except Exception as ex:
                 print(f"Error {ex}")
-            if run == 3:
+            print(f"Run {run} of 4")
+            if run == 4:
                 print("Aborting, limit reached.")
                 return False
             time.sleep(2)
