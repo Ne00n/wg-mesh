a7dc135b2e4be95599aa905e481313f5f02fa64b
Neoon <neo@tiefkuehler-madness.me>
2023-01-19 01:44:25 +0100

    nah

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 apt-get install wireguard bird2 sudo python3 python3-pip fping git -y
-pip install -r /opt/wg-mesh/requirements.txt
+pip3 install netaddr requests bottle
 cd /opt/
 #git
 git clone https://github.com/Ne00n/wg-mesh.git
