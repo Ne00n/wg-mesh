22474123a568831a4bf2e99e877bb8637ac94d1d
Neoon <neo@tiefkuehler-madness.me>
2023-03-19 17:29:30 +0100

    use getLogger

--- cron/bird.py
+++ cron/bird.py
@@ -13,7 +13,8 @@ level = "info"
 levels = {'critical': logging.CRITICAL,'error': logging.ERROR,'warning': logging.WARNING,'info': logging.INFO,'debug': logging.DEBUG}
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(levels[level])
-logger = logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%H:%M:%S',level=levels[level],handlers=[RotatingFileHandler(maxBytes=10000000,backupCount=5,filename=f"{path}/logs/network.log"),stream_handler])
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%H:%M:%S',level=levels[level],handlers=[RotatingFileHandler(maxBytes=10000000,backupCount=5,filename=f"{path}/logs/network.log"),stream_handler])
+logger = logging.getLogger()
 
 latency = Latency(path,logger)
 bird = Bird(path,logger)
