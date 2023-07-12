ed5c5840e18c290ba57f86e0c9b1ed9d6c064f49
Neoon <neo@tiefkuehler-madness.me>
2023-07-12 20:42:19 +0200

    only reload if thresholds are meet

--- Class/latency.py
+++ Class/latency.py
@@ -151,7 +151,7 @@ class Latency(Base):
             self.logger.warning("Nothing todo")
         else:
             #reload bird with updates only every 5 minutes or if packetloss is detected
-            if (datetime.now().minute % 5 == 0 and runs == 0) or self.hasLoss > 0 or self.hasJitter > 0:
+            if (datetime.now().minute % 5 == 0 and runs == 0) or self.hasLoss > 1 or self.hasJitter > 3:
                 #write
                 self.logger.info("Writing config")
                 self.cmd("echo '"+configRaw+"' > /etc/bird/bird.conf")
