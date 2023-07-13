96130f0c6927101eb11550aa2266b40276feb5db
Neoon <neo@tiefkuehler-madness.me>
2023-07-13 12:09:06 +0200

    update machine-id

--- tools/machine-id.py
+++ tools/machine-id.py
@@ -8,9 +8,8 @@ ids = {}
 for i in range(1,100):
     print(f"10.0.{i}.1","Checking machine-ID")
     resp = B.cmd(f"ssh root@10.0.{i}.1 cat /etc/machine-id",3)
-    if resp[0] == "": 
-        print("Err")
-        continue
+    if resp[0] == "": continue
+    print(f"Got {resp[0].rstrip()}")
     ids[i] = resp[0]
 
 currentIDs = []
