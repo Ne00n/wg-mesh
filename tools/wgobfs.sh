0bc31d8b741501c4a14631a4ec225c8203e85b05
Neoon <neo@tiefkuehler-madness.me>
2023-08-06 12:30:46 +0200

    need to install moah

--- tools/wgobfs.sh
+++ tools/wgobfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install linux-headers-$(uname -r) git autoconf libtool libxtables-dev pkg-config -y
+apt-get install linux-headers-$(uname -r) git make autoconf automake libtool libxtables-dev pkg-config -y
 #git clone https://github.com/Ne00n/xt_wgobfs.git
 git clone https://github.com/infinet/xt_wgobfs
 cd xt_wgobfs
