cf043d5498f66d166a81cafbb13ca08dc48e6521
Neoon <neo@tiefkuehler-madness.me>
2023-08-07 19:17:07 +0200

    typo fix

--- Class/wireguard.py
+++ Class/wireguard.py
@@ -370,7 +370,7 @@ class Wireguard(Base):
             req = self.call(f'http://{details["vxlan"]}:8080/disconnect',{"publicKeyServer":details['publicKey'],"interface":interfaceRemote})
             if req == False: continue
             if req.status_code == 200:
-                interface = self.filterInterface(filename)
+                interface = self.filterInterface(link)
                 self.setInterface(interface,"down")
                 self.cleanInterface(interface)
             else:
