0fb3e3d7e038ad7cf2d77eebe7b327b762b7e688
Neoon <neo@tiefkuehler-madness.me>
2023-08-24 02:33:53 +0200

    diagnostic base

---
+++ Class/diag.py
@@ -0,0 +1,42 @@
+import random, time, json, re, os
+from Class.wireguard import Wireguard
+from Class.base import Base
+
+class Diag(Base):
+
+    def __init__(self,path,logger):
+        self.logger = logger
+        self.wg = Wireguard(path)
+        self.path = path
+        file = f"{path}/configs/"
+        self.diagnostic = self.readConfig(f"{file}diagnostic.json")
+        self.network = self.readConfig(f"{file}network.json")
+
+    def run(self):
+        self.logger.info("Starting diagnostic")
+        if not os.path.isfile(f"{self.path}/configs/state.json"):
+            self.logger.warning("state.json does not exist")
+            exit()
+        targets = self.getRoutes()
+        if not targets: 
+            self.logger.warning("bird returned no routes, did you setup bird?")
+            exit()
+        links = self.wg.getLinks()
+        self.logger.info("Checking Links")
+        offline,online = self.wg.checkLinks(links)
+        for link in offline:
+            self.logger.info(f"Found dead link {link}")
+            count, data, current = 0, links[link], int(time.time())
+            parsed, remote = self.getRemote(data['local'])
+            if not remote in self.diagnostic: self.diagnostic[remote] = {"cooldown":0}
+            if current < self.diagnostic[remote]['cooldown']: continue
+            self.diagnostic[remote]['cooldown'] = current + 3600
+            for event,lost in list(self.network[remote]['packetloss'].items()):
+                if int(event) > int(time.time()) and lost == 4: count += 1 
+            if count < 20: continue
+            endpoint = f"{parsed[1]}1"
+            pings = self.fping([endpoint],3,True)
+            if not pings[endpoint]:
+                self.logger.info(f"Unable to reach endpoint {endpoint} for link {link}")
+                continue
+            self.logger.info(f"Dead link confirmed {link}")
---
+++ cron/diag.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import logging, time, sys, os
+sys.path.append("..") # Adds higher directory to python modules path.
+from logging.handlers import RotatingFileHandler
+from Class.diag import Diag
+
+path = os.path.dirname(os.path.realpath(__file__))
+path = path.replace("/cron","")
+
+#logging
+level = "debug"
+levels = {'critical': logging.CRITICAL,'error': logging.ERROR,'warning': logging.WARNING,'info': logging.INFO,'debug': logging.DEBUG}
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(levels[level])
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%d.%m.%Y %H:%M:%S',level=levels[level],handlers=[RotatingFileHandler(maxBytes=10000000,backupCount=5,filename=f"{path}/logs/diagnostic.log"),stream_handler])
+logger = logging.getLogger()
+
+diag = Diag(path,logger)
+diag.run()
