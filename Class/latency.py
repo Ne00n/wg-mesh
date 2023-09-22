0883b18454f6acb1028e4794718aec57600880e8
Neoon <neo@tiefkuehler-madness.me>
2023-09-22 12:39:58 +0200

    since current is already set add

--- Class/latency.py
+++ Class/latency.py
@@ -80,7 +80,7 @@ class Latency(Base):
 
                     eventCount,eventScore = self.countEvents(entry,'packetloss')
                     if eventCount > 0:
-                        node['latency'] = round(node['current'] + (eventScore * self.multiplicator))
+                        node['latency'] += round((eventScore * self.multiplicator))
                         self.logger.debug(f"Loss {node['nic']} ({entry}) Latency: {node['current']}, Modified: {node['latency']}, Score: {eventScore}, Count: {eventCount}")
                         if self.reloadPeacemaker(hasLoss,eventCount,node['latency'],node['weight']): 
                             self.logger.debug(f"{node['nic']} ({entry}) Triggering Packetloss reload")
