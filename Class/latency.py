38b0e9d513544ee1e92e6cb86e32e77fe16d1ed9
Neoon <neo@tiefkuehler-madness.me>
2023-09-26 23:39:56 +0200

    remove jitter score multiplier

--- Class/latency.py
+++ Class/latency.py
@@ -79,7 +79,6 @@ class Latency(Base):
                         self.logger.info(f"{node['nic']} ({entry}) Packetloss detected got {len(row)} of {pings -1}")
 
                     eventCount,eventScore = self.countEvents(entry,'packetloss')
-                    eventScore = round(eventScore * 10)
                     if eventCount > 0:
                         node['latency'] += round((eventScore * self.multiplicator))
                         self.logger.debug(f"Loss {node['nic']} ({entry}) Latency: {node['current']}, Modified: {node['latency']}, Score: {eventScore}, Count: {eventCount}")
