5ccba919ed29241ddab7b41aa26e972799717929
Neoon <neo@tiefkuehler-madness.me>
2024-02-18 00:25:29 +0100

    remove merge

--- Class/templator.py
+++ Class/templator.py
@@ -146,7 +146,6 @@ filter export_OSPF {
 protocol ospf {
     tick '''+str(config['bird']['tick'])+''';
     graceful restart yes;
-    merge external yes;
     stub router '''+isRouter+''';
     ipv4 {
         import all;
