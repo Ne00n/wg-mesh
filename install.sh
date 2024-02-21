76c49b7f7b34a1a9baf4c1e0dcc8193a728f674d
Neoon <neo@tiefkuehler-madness.me>
2024-02-21 06:30:15 +0100

    requests via apt-get

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-apt-get install wireguard iptables bird2 sudo python3 python3-netaddr python3-pip fping mtr vnstat git -y
-pip3 install requests bottle paste
+apt-get install wireguard iptables bird2 sudo python3 python3-netaddr python3-requests python3-pip fping mtr vnstat git -y
+pip3 install bottle paste
 cd /opt/
 #git
 git clone https://github.com/Ne00n/wg-mesh.git
