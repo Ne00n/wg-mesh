6b1d3821dcd4e02a474664337d3e6968c0f940b7
Neoon <neo@tiefkuehler-madness.me>
2024-04-01 16:30:01 +0200

    update clean

--- tools/clean.py
+++ tools/clean.py
@@ -1,14 +1,21 @@
-import sys
+import socket, sys
 sys.path.append("..") # Adds higher directory to python modules path.
 
 from Class.base import Base
 B = Base()
 
-for i in range(1,100):
+for i in range(1,250):
+    #check if SSH is reachable
+    print("Checking,"f"10.0.{i}.1")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    try: s.connect((f"10.0.{i}.1", 22)) 
+    except: continue 
+    #continue
     print("Cleaning",f"10.0.{i}.1")
     resp = B.cmd(f"""ssh root@10.0.{i}.1 <<EOF
 /opt/wg-mesh/cli.py clean
 EOF""",120)
-    print(resp)
+    for line in resp: print(line)
 
 print("END")
