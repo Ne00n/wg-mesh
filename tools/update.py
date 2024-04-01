d8dc8da10b9c95c93c69e872dd38f70c05af8c1b
Neoon <neo@tiefkuehler-madness.me>
2024-04-01 16:46:47 +0200

    update update

--- tools/update.py
+++ tools/update.py
@@ -1,10 +1,17 @@
-import sys
+import socket, sys
 sys.path.append("..") # Adds higher directory to python modules path.
 
 from Class.base import Base
 B = Base()
 
-for i in range(1,100):
+for i in range(1,250):
+    #check if SSH is reachable
+    print("Checking,"f"10.0.{i}.1")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    try: s.connect((f"10.0.{i}.1", 22)) 
+    except: continue 
+    #continue
     print("Updating",f"10.0.{i}.1")
     resp = B.cmd(f"""ssh root@10.0.{i}.1 <<EOF
 su wg-mesh
@@ -14,7 +21,7 @@ python3 cli.py migrate
 exit
 systemctl restart wgmesh
 systemctl restart wgmesh-bird
-EOF""",10)
+EOF""",60)
     print(resp)
 
 print("END")
