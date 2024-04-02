777b139ef50ada3eba3243c17527a48c9e3c1f15
Neoon <neo@tiefkuehler-madness.me>
2024-04-02 19:16:40 +0200

    template fix

--- Class/templator.py
+++ Class/templator.py
@@ -7,8 +7,8 @@ class Templator:
         wgobfs,mtu = "",1412 if "v6" in interface else 1420
         if linkType == "wgobfs": wgobfs += f"sudo iptables -t mangle -I INPUT -p udp -m udp --dport {serverPort} -j WGOBFS --key {wgobfsSharedKey} --unobfs;\n"
         if linkType == "wgobfs": wgobfs += f"sudo iptables -t mangle -I OUTPUT -p udp -m udp --sport {serverPort} -j WGOBFS --key {wgobfsSharedKey} --obfs;\n"
-        if linkType == "ipt_xor": wgobfs += f"sudo iptables -t mangle -A OUTPUT -p udp --dport {serverPort} -j XOR --keys "{wgobfsSharedKey}";\n"
-        if linkType == "ipt_xor": wgobfs += f"sudo iptables -t mangle -A INPUT -p udp --sport {serverPort} -j XOR --keys "{wgobfsSharedKey}";\n"
+        if linkType == "ipt_xor": wgobfs += f'sudo iptables -t mangle -A OUTPUT -p udp --dport {serverPort} -j XOR --keys "{wgobfsSharedKey}";\n'
+        if linkType == "ipt_xor": wgobfs += f'sudo iptables -t mangle -A INPUT -p udp --sport {serverPort} -j XOR --keys "{wgobfsSharedKey}";\n'
         wgobfsReverse = wgobfs.replace("mangle -I","mangle -D")
         template = f'''#!/bin/bash
 #Area {area}
@@ -31,8 +31,8 @@ fi'''
         wgobfs,mtu = "",1412 if "v6" in interface else 1420
         if linkType == "wgobfs": wgobfs += f"sudo iptables -t mangle -I INPUT -p udp -m udp --sport {serverPort} -j WGOBFS --key {wgobfsSharedKey} --unobfs;\n"
         if linkType == "wgobfs": wgobfs += f"sudo iptables -t mangle -I OUTPUT -p udp -m udp --dport {serverPort} -j WGOBFS --key {wgobfsSharedKey} --obfs;\n"
-        if linkType == "ipt_xor": wgobfs += f"sudo iptables -t mangle -A OUTPUT -p udp --dport {serverPort} -j XOR --keys "{wgobfsSharedKey}";\n"
-        if linkType == "ipt_xor": wgobfs += f"sudo iptables -t mangle -A INPUT -p udp --sport {serverPort} -j XOR --keys "{wgobfsSharedKey}";\n"
+        if linkType == "ipt_xor": wgobfs += f'sudo iptables -t mangle -A OUTPUT -p udp --dport {serverPort} -j XOR --keys "{wgobfsSharedKey}";\n'
+        if linkType == "ipt_xor": wgobfs += f'sudo iptables -t mangle -A INPUT -p udp --sport {serverPort} -j XOR --keys "{wgobfsSharedKey}";\n'
         wgobfsReverse = wgobfs.replace("mangle -I","mangle -D")
         template = f'''#!/bin/bash
 #Area {config['bird']["area"]}
