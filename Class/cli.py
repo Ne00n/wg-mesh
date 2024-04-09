431d04f70fa36aadd2545d3c439ce5a95433221f
Neoon <neo@tiefkuehler-madness.me>
2024-04-10 01:28:03 +0200

    set Debug

--- Class/cli.py
+++ Class/cli.py
@@ -55,7 +55,7 @@ class CLI(Base):
 
     def recover(self):
         stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(levels[level])
+        stream_handler.setLevel(logging.DEBUG)
         logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%d.%m.%Y %H:%M:%S',level=levels[level],handlers=[RotatingFileHandler(maxBytes=10000000,backupCount=5,filename=f"{path}/logs/recovery.log"),stream_handler])
         logger = logging.getLogger()
         self.bird = Bird(self.path,logger)
