389e585d1895af041e7ab39f310b5bad338281cb
Neoon <neo@tiefkuehler-madness.me>
2024-06-09 17:48:33 +0200

    install mtr-tiny instead

--- installLXC.sh
+++ installLXC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-apt-get install wireguard iptables bird2 sudo python3 python3-netaddr python3-paste python3-bottle python3-requests python3-pip fping mtr vnstat git -y
+apt-get install wireguard iptables bird2 sudo python3 python3-netaddr python3-paste python3-bottle python3-requests python3-pip fping mtr-tiny vnstat git -y
 cd /opt/
 #git
 git clone https://github.com/Ne00n/wg-mesh.git
