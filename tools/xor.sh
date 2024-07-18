1fdbef9cb9336f3163dd1a8eb0abe48a8620b7f4
Neoon <neo@tiefkuehler-madness.me>
2024-07-18 04:09:25 +0200

    update xor.sh

--- tools/xor.sh
+++ tools/xor.sh
@@ -3,6 +3,7 @@ cd
 apt-get update
 apt-get install linux-headers-$(uname -r) git make autoconf automake libtool libxtables-dev pkg-config -y
 git clone https://github.com/faicker/ipt_xor
+#https://github.com/Ne00n/ipt_xor
 cd ipt_xor
 cd userspace
 make libxt_XOR.so
@@ -12,3 +13,4 @@ cd kernel
 make
 insmod xt_XOR.ko
 echo 'ipt_xor' >> /etc/modules
+lsmod | grep "xor"
