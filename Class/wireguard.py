17c5e2714092f27c6b9f78a05e55f14641e7207d
Neoon <neo@tiefkuehler-madness.me>
2024-07-21 00:22:15 +0200

    wireguard load public IP from links

--- Class/wireguard.py
+++ Class/wireguard.py
@@ -173,8 +173,10 @@ class Wireguard(Base):
             if not filename.endswith(".sh") or filename == "dummy.sh": continue
             with open(f"{self.path}/links/{filename}", 'r') as file: config = file.read()
             subnetPrefix,subnetPrefixSplitted = self.subnetSwitch(filename)
+            remotePublic = ""
             #grab wg server ip from client wg config
             if "endpoint" in config:
+                remotePublic = re.findall(f'endpoint\s(.*):',config, re.MULTILINE)[0]
                 destination = re.findall(f'({subnetPrefixSplitted[0]}\.{subnetPrefixSplitted[1]}\.[0-9]+\.)',config, re.MULTILINE)
                 if not destination:
                     print(f"Ignoring {filename}")
@@ -197,7 +199,7 @@ class Wireguard(Base):
             #grab area
             area = re.findall(f"Area\s([0-9]+)",config,re.MULTILINE)
             area = int(area[0]) if area else 0
-            links[filename] = {"filename":filename,"vxlan":destination,"local":parsed[0],"remote":remote,'publicKey':publicKey,"area":area}
+            links[filename] = {"filename":filename,"vxlan":destination,"local":parsed[0],"remote":remote,'remotePublic':remotePublic,'publicKey':publicKey,"area":area}
         return links
 
     def AskProtocol(self,dest,token=""):
