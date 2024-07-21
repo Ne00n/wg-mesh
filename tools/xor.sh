933d3f5027eb54c9ad552939b37d94a4ca30d59b
Neoon <neo@tiefkuehler-madness.me>
2024-07-22 01:53:44 +0200

    git pull when repo already exists

--- tools/xor.sh
+++ tools/xor.sh
@@ -3,9 +3,14 @@ set -e
 cd
 apt-get update
 apt-get install linux-headers-$(uname -r) git make autoconf automake libtool libxtables-dev pkg-config -y
-git clone https://github.com/faicker/ipt_xor
-#https://github.com/Ne00n/ipt_xor
-cd ipt_xor
+if [ ! -d "ipt_xor" ] ; then
+    git clone https://github.com/faicker/ipt_xor
+    #https://github.com/Ne00n/ipt_xor
+    cd ipt_xor
+else
+    cd "ipt_xor"
+    git pull
+fi
 cd userspace
 make libxt_XOR.so
 cp libxt_XOR.so /usr/lib/x86_64-linux-gnu/xtables/
