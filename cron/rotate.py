a945fb34a1f6527ee909875adb1f2286ca0383cd
Neoon <neo@tiefkuehler-madness.me>
2024-07-23 21:11:45 +0200

    add rotate cron

---
+++ cron/rotate.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+import logging, secrets, time, sys, os
+sys.path.append("..") # Adds higher directory to python modules path.
+from logging.handlers import RotatingFileHandler
+from Class.wireguard import Wireguard
+
+path = os.path.dirname(os.path.realpath(__file__))
+path = path.replace("/cron","")
+
+#logging
+level = "info"
+levels = {'critical': logging.CRITICAL,'error': logging.ERROR,'warning': logging.WARNING,'info': logging.INFO,'debug': logging.DEBUG}
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(levels[level])
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%d.%m.%Y %H:%M:%S',level=levels[level],handlers=[RotatingFileHandler(maxBytes=10000000,backupCount=5,filename=f"{path}/logs/rotate.log"),stream_handler])
+logger = logging.getLogger()
+
+wg = Wireguard(path)
+config = wg.getConfig()
+path,links = f'{path}/links/',[]
+
+waitUntil = 0
+while True:
+    currentTime = int(time.time())
+    if currentTime > waitUntil:
+        links = wg.getLinks()
+        for link, data in links.items():
+            if "XOR" in data['config']:
+                link = wg.filterInterface(link)
+                logger.info(f"{link} swapping xor keys")
+                logger.info(f"{link} increasing cost")
+                result = wg.setCost(link,5000)
+                if not result:
+                    logger.info(f"Failed to increase cost")
+                    continue
+                logger.info(f"{link} waiting 30s for cost to apply")
+                time.sleep(30)
+                logger.info(f"{link} shutting link down")
+                wg.setInterface(link,"down")
+                logger.info(f"{link} updating remote xor keys")
+                interfaceRemote = wg.getInterfaceRemote(link)
+                xorKey = secrets.token_urlsafe(24)
+                req = wg.call(f'http://{data["vxlan"]}:{config["listenPort"]}/update',{"xorKey":xorKey,"publicKeyServer":data['publicKey'],"interface":interfaceRemote},'PATCH')
+                if not req:
+                    logger.info(f"{link} Failed to update remote xor keys")
+                    logger.info(f"{link} restoring link state")
+                    wg.setCost(link,0)
+                    wg.setInterface(link,"up")
+                    continue
+                logger.info(f"{link} updating local xor keys")
+                wg.updateLink(link,{'xorKey':xorKey})
+                logger.info(f"{link} starting link")
+                wg.setInterface(link,"up")
+                logger.info(f"{link} removing cost")
+                wg.setCost(link,0)
+                logger.info(f"{link} done swapping xor keys")
+        #run this once per hour
+        waitUntil = currentTime + 3600
+    else:
+        time.sleep(30)
