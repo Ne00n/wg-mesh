04d2ecdea326e2a1e03be8cea456ec5e6dd3ac45
Neoon <neo@tiefkuehler-madness.me>
2024-07-23 14:09:23 +0200

    add reinstall.sh for dependencies reinstall

---
+++ reinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+apt-get install wireguard iptables bird2 sudo python3 python3-netaddr python3-paste python3-bottle python3-requests python3-pip fping mtr-tiny vnstat git -y
