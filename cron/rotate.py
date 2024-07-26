db52887753de0967989b03e47f80721f9ffa0ec3
Neoon <neo@tiefkuehler-madness.me>
2024-07-27 00:45:31 +0200

    300 dang it

--- cron/rotate.py
+++ cron/rotate.py
@@ -44,7 +44,7 @@ while True:
                     logger.info(f"Failed to increase cost")
                     continue
                 logger.info(f"{link} waiting 300s for cost to apply")
-                time.sleep(500)
+                time.sleep(300)
                 logger.info(f"{link} shutting link down")
                 wg.setInterface(link,"down")
                 logger.info(f"{link} updating remote xor keys")
