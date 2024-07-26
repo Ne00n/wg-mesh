18302fc6f7ae45bcfc3010d09a294d4096c69e37
Neoon <neo@tiefkuehler-madness.me>
2024-07-26 22:40:20 +0200

    add gotifyError

--- Class/wireguard.py
+++ Class/wireguard.py
@@ -34,6 +34,7 @@ class Wireguard(Base):
         if not "client" in self.config['bird']: self.config['bird']['client'] = False
         if not "vxlan" in self.config: self.config['vxlan'] = 251
         if not "notifications" in self.config: self.config['notifications'] = {"enabled":False,"gotifyUp":"","gotifyDown":""}
+        if not "gotifyError" in self.config['notifications']: self.config['notifications']['gotifyError'] = ""
         self.saveJson(self.config,f"{self.path}/configs/config.json")
 
     def genKeys(self):
@@ -87,7 +88,7 @@ class Wireguard(Base):
         connectivity = {"ipv4":ipv4,"ipv6":ipv6}
         config = {"listen":listen,"listenPort":8080,"basePort":51820,"subnet":"10.0.0.0/16","subnetPeer":"172.31.0.0/16","AllowedPeers":[],
         "prefix":"pipe","id":id,'vxlan':251,"linkTypes":["default"],"defaultLinkType":"default","connectivity":connectivity,
-        "bird":{"ospfv2":True,"ospfv3":True,"area":0,"tick":1,"client":False},"notifications":{"enabled":False,"gotifyUp":"","gotifyDown":""}}
+        "bird":{"ospfv2":True,"ospfv3":True,"area":0,"tick":1,"client":False},"notifications":{"enabled":False,"gotifyUp":"","gotifyDown":"","gotifyError":""}}
         response = self.saveJson(config,f"{self.path}/configs/config.json")
         if not response: exit("Unable to save config.json")
         #load configs
