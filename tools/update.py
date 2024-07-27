47b0d5aac9eae6ba6c6765474587be1b380bf4d8
Neoon <neo@tiefkuehler-madness.me>
2024-07-27 14:21:21 +0200

    disable that for now

--- tools/update.py
+++ tools/update.py
@@ -21,7 +21,7 @@ python3 cli.py migrate
 exit
 systemctl restart wgmesh
 systemctl restart wgmesh-bird
-systemctl restart wgmesh-rotate
+#systemctl restart wgmesh-rotate
 EOF""",60)
     print(resp)
 
