41df87a3930823e53d32647ef77d4b1124ae7973
Neoon <neo@tiefkuehler-madness.me>
2024-08-09 14:34:12 +0200

    status check peer

--- tools/status.py
+++ tools/status.py
@@ -1,11 +1,14 @@
-import sys
+import socket, sys
 sys.path.append("..") # Adds higher directory to python modules path.
 
 from Class.base import Base
 B = Base()
 
-for i in range(1,100):
-    print("Checking",f"10.0.{i}.1")
+for i in range(1,250):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    try: s.connect((f"10.0.{i}.1", 22)) 
+    except: continue 
     resp = B.cmd(f"""ssh root@10.0.{i}.1 <<EOF
 systemctl status wgmesh-bird
 EOF""",10)
