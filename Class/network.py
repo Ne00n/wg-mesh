d3cd7dd7d68a9d0dd5c4de61c2109e9ecf7f98ff
Neoon <neo@tiefkuehler-madness.me>
2024-12-13 21:24:51 +0100

    use /116 + getPeerSubnetsv6

--- Class/network.py
+++ Class/network.py
@@ -13,7 +13,7 @@ class Network:
             return f"{self.subnetPrefix}.{self.config['id']}.0/24"
 
     def getNodeSubnetv6(self):
-        return f"fe82:{self.config['id']}::/32"
+        return f"fe82:{self.config['id']}::/116"
 
     def getPeerSubnets(self):
         nodeSubnet = self.getNodeSubnet()
@@ -21,3 +21,8 @@ class Network:
         subnets = list(network.subnets(new_prefix=31))
         subnets = subnets[2:]
         return subnets
+
+    def getPeerSubnetsv6(self):
+        nodeSubnet = self.getNodeSubnetv6()
+        network = ipaddress.ip_network(nodeSubnet)
+        return list(network.subnets(new_prefix=127))
