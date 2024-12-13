4bb922bb90afaf93eb12503542f001d37e9b0343
Neoon <neo@tiefkuehler-madness.me>
2024-12-13 11:49:35 +0100

    start from 0

--- tools/clean.py
+++ tools/clean.py
@@ -4,7 +4,7 @@ sys.path.append("..") # Adds higher directory to python modules path.
 from Class.base import Base
 B = Base()
 
-for i in range(1,250):
+for i in range(0,250):
     #check if SSH is reachable
     print("Checking,"f"10.0.{i}.1")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
