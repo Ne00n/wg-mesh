ae89016b27468fc671175dadd5fb384630dfaf0f
Neoon <neo@tiefkuehler-madness.me>
2024-12-22 01:36:34 +0100

    geo if fix

--- api.py
+++ api.py
@@ -112,7 +112,7 @@ def index():
     if not isInternal and not validateToken(payload): 
         logging.info(f"Invalid Token from {requestIP}")
         return HTTPResponse(status=401, body="Invalid Token")
-    geo = config['geo'] if config['geo'] else {}
+    geo = config['geo'] if "geo" in config else {}
     return HTTPResponse(status=200, body={'connectivity':config['connectivity'],'geo':geo,'linkTypes':config['linkTypes'],'subnetPrefix':subnetPrefix})
 
 @route('/connect', method='POST')
