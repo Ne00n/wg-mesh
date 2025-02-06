e60144ef3a00e19a6bef49af55dcbb57dc052f94
Neoon <neo@tiefkuehler-madness.me>
2025-02-06 07:11:28 +0100

    diag add systemd support

--- cron/diag.py
+++ cron/diag.py
@@ -3,6 +3,7 @@ import logging, time, sys, os
 sys.path.append("..") # Adds higher directory to python modules path.
 from logging.handlers import RotatingFileHandler
 from Class.diag import Diag
+import systemd.daemon
 
 path = os.path.dirname(os.path.realpath(__file__))
 path = path.replace("/cron","")
@@ -15,8 +16,20 @@ stream_handler.setLevel(levels[level])
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%d.%m.%Y %H:%M:%S',level=levels[level],handlers=[RotatingFileHandler(maxBytes=10000000,backupCount=5,filename=f"{path}/logs/diagnostic.log"),stream_handler])
 logger = logging.getLogger()
 
+shutdown = False
+def gracefulExit(signal_number,stack_frame):
+    systemd.daemon.notify('STOPPING=1')
+    logger.info(f"Stopping")
+    global shutdown
+    shutdown = True
+
+signal.signal(signal.SIGINT, gracefulExit)
+signal.signal(signal.SIGTERM, gracefulExit)
+systemd.daemon.notify('READY=1')
+logger.info(f"Ready")
+
 diag = Diag(path,logger)
-while True:
+while not shutdown:
     #check for lock file
     if os.path.isfile(f"{path}/cron/lock"): 
         time.sleep(60)
