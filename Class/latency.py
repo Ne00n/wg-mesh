6854e5602496cebae4cf6dc2cf6a685ea22b9a63
Neoon <neo@tiefkuehler-madness.me>
2025-03-21 20:25:36 +0100

    typo fix

--- Class/latency.py
+++ Class/latency.py
@@ -282,7 +282,7 @@ class Latency(Base):
                     messages['down'].append([0,row])
             #if the difference suddenly is bigger than or equal 20ms, trigger an mtr + ignore negative changes
             elif diff >= 20 and diff <= 2000 and diff > 0:
-                self.logger.debug(f"{nic} got {diff}ms change, before {round(row['cost'] / 10)}ms, now {round(oldLatencyData['cost'] / 10)}ms")
+                self.logger.debug(f"{nic} got {diff}ms change, before {round(row['cost'] / 10)}ms, now {round(oldRow['cost'] / 10)}ms")
                 if notifications['enabled'] and notifications['gotifyChanges'] and notifications['gotifyChanges'] != "disabled":
                     messages['changes'].append([2,row,oldRow])
         #processing gotify messages
