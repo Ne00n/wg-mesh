d7494a5840dc11be92afa66f4009b2f3e3a9991a
Neoon <neo@tiefkuehler-madness.me>
2025-03-24 15:55:04 +0100

    smoke geo fix

--- cron/smoke.py
+++ cron/smoke.py
@@ -21,7 +21,7 @@ for index, target in enumerate(targets):
     print(f"Getting {index} of {len(targets) -1}")
     resp = wireguard.AskProtocol(f'http://{target}:8080','')
     if not resp: continue
-    if not "geo" in resp: 
+    if not "geo" in resp or not resp['geo']: 
         print(f"No geo from {target}")
         continue
     if not resp: 
