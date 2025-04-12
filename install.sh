b94ee8e568feac3479f4e32d2f85b7b5cd02089a
Neoon <neo@tiefkuehler-madness.me>
2025-04-12 04:00:13 +0200

    enable diag by default

--- install.sh
+++ install.sh
@@ -59,3 +59,6 @@ systemctl enable wgmesh-pipe
 #systemd roate service
 cp /opt/wg-mesh/configs/wgmesh-rotate.service /etc/systemd/system/wgmesh-rotate.service
 systemctl enable wgmesh-rotate && systemctl start wgmesh-rotate
+#systemd diag service
+cp /opt/wg-mesh/configs/wgmesh-diag.service /etc/systemd/system/wgmesh-diag.service
+systemctl enable wgmesh-diag && systemctl start wgmesh-diag
