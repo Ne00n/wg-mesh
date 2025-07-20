282e2a3c974cd60e64f3d27d096d13894256e221
Neoon <neo@tiefkuehler-madness.me>
2025-07-20 05:41:53 +0200

    Network include networkID

--- Class/network.py
+++ Class/network.py
@@ -7,8 +7,10 @@ class Network:
         self.subnetPrefix = ".".join(self.config['subnet'].split(".")[:2])
 
     def getNodeSubnet(self):
-        if self.config['subnet'].startswith("10."):
+        if self.config['subnet'].startswith("10.") and self.config['networkID'] == 0:
             return f"{self.subnetPrefix}.{self.config['id']}.0/23"
+        elif self.config['subnet'].startswith("10."):
+            return f"10.{self.config['networkID']}.{self.config['id']}.0/23"
         else:
             return f"{self.subnetPrefix}.{self.config['id']}.0/24"
 
