c4f92ab871cfd47dc18122ec6ecb69310232b747
Neoon <neo@tiefkuehler-madness.me>
2025-07-23 06:01:26 +0200

    rename status check

--- Class/cli.py
+++ Class/cli.py
@@ -33,7 +33,7 @@ class CLI(Base):
             dest = f"http://{subnetPrefix}.{pipeTarget[0]}.1:{config['listenPort']}"
         status = self.wg.connect(dest,token,linkType,port,network)
         if self.wg.getInitial:
-            if not status['v4']['status'] and not status['v6']['status']:
+            if not status['ipv4']['status'] and not status['ipv6']['status']:
                 print(f"Initial link wasn't setup.")
                 return 
             print("Waiting for meshing to complete.")
