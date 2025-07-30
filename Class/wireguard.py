33f8dbcfac1a3725bdf8547793d226eb0948c953
Neoon <neo@tiefkuehler-madness.me>
2025-07-30 21:35:35 +0200

    wg add createInterface

--- Class/wireguard.py
+++ Class/wireguard.py
@@ -186,6 +186,13 @@ class Wireguard(Base):
             if os.path.isfile(f"{self.path}/links/{interface}.pre"): os.remove(f"{self.path}/links/{interface}.pre")
             if os.path.isfile(f"{self.path}/links/{interface}.json"): os.remove(f"{self.path}/links/{interface}.json")
 
+    def createInterface(self,interface,clientPrivateKey,preSharedKey,clientConfig,linkConfig):
+        self.saveFile(clientPrivateKey,f"{self.path}/links/{interface}.key")
+        self.saveFile(preSharedKey,f"{self.path}/links/{interface}.pre")
+        self.saveFile(clientConfig,f"{self.path}/links/{interface}.sh")
+        self.saveJson(linkConfig,f"{self.path}/links/{interface}.json")
+        self.getInterface(interface,"up")
+
     def removeInterface(self,interface):
         self.setInterface(interface,"down")
         self.cleanInterface(interface)
