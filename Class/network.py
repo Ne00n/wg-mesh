b9a0d95dd5b082492f46b15543212e28ce3b0b1e
Neoon <neo@tiefkuehler-madness.me>
2025-08-03 15:52:05 +0200

    check for tunnel in network

--- Class/network.py
+++ Class/network.py
@@ -17,7 +17,7 @@ class Network(Base):
         return self.config[selector].split(".")
 
     def subnetSwitch(self,network=""):
-        if "Peer" in network:
+        if "Peer" in network or "tunnel" in network:
             return self.subnetPeerSplitted,self.subnetPeerPrefix
         else:
             return self.subnetSplitted,self.subnetPrefix
