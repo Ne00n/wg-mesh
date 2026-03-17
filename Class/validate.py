cf8f49f4cd519216643c118e3e4e43baed82d65c
Ne00n <neo@tiefkuehler-madness.me>
2026-03-17 13:14:00 +0100

    ups

--- Class/validate.py
+++ Class/validate.py
@@ -7,28 +7,28 @@ class Validate():
         if not result: return False
         return True
 
-    def port(port):
+    def port(self,port):
         result = re.findall(r"^[0-9]{4,5}$",str(port),re.MULTILINE | re.DOTALL)
         if not result: return False
         return True
 
-    def network(network):
+    def network(self,network):
         result = re.findall(r"^[A-Za-z]{3,6}$",network,re.MULTILINE | re.DOTALL)
         if not result: return False
         return True
 
-    def linkType(linkType,config):
+    def linkType(self,linkType,config):
         linkTypes = ["default","wgobfs","ipt_xor","amneziawg"]
         if not linkType in linkTypes: return False
         if not linkType in config['linkTypes']: return False
         return True
 
-    def prefix(prefix):
+    def prefix(self,prefix):
         result = re.findall(r"^[0-9.]{4,6}$",prefix,re.MULTILINE | re.DOTALL)
         if not result: return False
         return True
 
-    def token(payload,tokens):
+    def token(self,payload,tokens):
         if not "token" in payload: return False
         token = re.findall(r"^([A-Za-z0-9/.=+]{18,60})$",payload['token'],re.MULTILINE | re.DOTALL)
         if not token: return False
