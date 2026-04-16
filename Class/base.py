4eaa0a8db0cec11d5aef6f39c348975162a977d2
Ne00n <neo@tiefkuehler-madness.me>
2026-04-16 20:51:35 +0200

    fix processSubnet

--- Class/base.py
+++ Class/base.py
@@ -160,7 +160,8 @@ class Base:
             batch = targets[i:i+10]
             result = self.fping(batch)
             if not result: continue
-            for row in result:
-                results.append([row[0].split(".")[-1],float(row[1])])
+            for subnet, details in result.items():
+                avg = self.getAvrg(details)
+                results.append([subnet.split(".")[-1],int(avg)])
             if not "any" in data['details']['settings']: break
         return {data['subnet']:results}
