61fdc358bce1073bcd162086f086cbf0be1c164c
Ne00n <neo@tiefkuehler-madness.me>
2026-04-19 01:54:00 +0200

    reduce to 5

--- cron/routing.py
+++ cron/routing.py
@@ -37,7 +37,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',datefmt='%d.%
 logger = logging.getLogger()
 
 gateway = tools.cmd("ip route show default | awk '/default via / {print $3; exit}' | tr -d '\n'")[0]
-config = {"dataSrc": "https://routing.serv.app","cutOff":10,"asnList": {"32590":{}}}
+config = {"dataSrc": "https://routing.serv.app","cutOff":5,"asnList": {"32590":{}}}
 if not os.path.isfile(f"{path}/configs/asn.json"):
     with open(f"{path}/configs/asn.json", 'w') as f: json.dump(config, f, indent=4)
 else:
