7fde880564575825ce0d6b20b67a96d71e9b277f
Ne00n <neo@tiefkuehler-madness.me>
2026-05-19 20:48:30 +0200

    proximity add index cutoff

--- Class/wireguard.py
+++ Class/wireguard.py
@@ -437,15 +437,15 @@ class Wireguard(Base):
         terminate, result = [], []
         result.append("Target\tIP address\tCity\tConnected\tLatency")
         result.append("-------\t-------\t-------\t-------\t-------")
-        for ip,latency in latencyData.items(): 
-            if latency > float(cutoff): terminate.append(mapping[ip]['target'])
+        for index, ip,latency in enumerate(latencyData.items()):
+            if (cutoff == 0 and index > 5) or (cutoff != 0 and latency > float(cutoff)): terminate.append(mapping[ip]['target'])
             result.append(f"{mapping[ip]['target']}\t{ip}\t{mapping[ip]['location']}\t{bool(ip in existing)}\t{format(latency,'.2f')}ms")
         result = self.formatTable(result)
         if cutoff == 0: 
             print(result)
             return True
-        for ip,latency in latencyData.items():
-            if latency > float(cutoff): continue 
+        for index, ip,latency in enumerate(latencyData.items()):
+            if (cutoff == 0 and index > 5) or (cutoff != 0 and latency > float(cutOff)): continue 
             targetSplit = mapping[ip]['target'].split(".")
             #reserve 10.0.200+ for clients, don't mesh
             if int(targetSplit[2]) >= 200: continue
